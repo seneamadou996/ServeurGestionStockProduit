ed075c7911670764038f3c3953c95bf81219ab62
Amadou Sene <seneamadou996@gmail.com>
2020-09-25 20:05:30 +0000

    first commit

---
+++ src/main/java/com/tutoriels/ServeurGestionStockProduitApplication.java
@@ -0,0 +1,13 @@
+package com.tutoriels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutoriels/ServeurGestionStockProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoriels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
